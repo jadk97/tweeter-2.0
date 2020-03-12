8672a2ba058862c675c89bcb51a01385ef123c60
Jad K <jad_dis@hotmail.com>
2020-03-12 03:00:57 -0400

    added a reusable button and fleshed out the UI of the compose tweet section a bit more

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css"
+const Button = (props) => {
+  return(
+    <React.Fragment>
+    <button className="button">{props.children}</button>
+    </React.Fragment>
+  )
+}
+
+export default Button;
--- src/components/ComposeTweet.js
+++ src/components/ComposeTweet.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ComposeTweet.css";
 import TextareaAutosize from 'react-textarea-autosize';
+import Button from "./Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImages } from '@fortawesome/free-solid-svg-icons'
 const ComposeTweet = (props) => {
+  const [tweet, setTweet] = useState("");
   return (
     <div className="compose-tweet">
       <form>
         <div className="tweet-box">
           <img className="avatar" src={"https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2017/04/04/604513.jpg?itok=FqqfYOfA"} />
-          <TextareaAutosize className="tweet-input" placeholder="What's on your mind?"></TextareaAutosize>
+          <TextareaAutosize 
+          className="tweet-input" 
+          placeholder="What's on your mind?"
+
+          ></TextareaAutosize>
+         
+        </div>
+        <div className="tweet-submit-options">
+        <div className="tweet-submit-image-icon">
+        <FontAwesomeIcon icon={faImages} size="2x" color="#1da1f2" />
+        </div>
+        <Button>Tweet</Button>
         </div>
       </form>
     </div>
