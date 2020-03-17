3c82177b471dc77f510d70994276ba81da3bacd8
Jad K <jad_dis@hotmail.com>
2020-03-17 04:05:47 -0400

    moved much of the logic into its own self-contained hook

--- src/App.js
+++ src/App.js
@@ -9,57 +9,10 @@ import SideBar from "./components/SideBar";
 import ComposeTweet from "./components/ComposeTweet";
 import Header from "./components/Header";
 import MainNavigation from './components/MainNavigation';
+
+import { useTweetInteract } from "./hooks/tweet-interact-hook";
 function App() {
-  const TWEETS = [
-    {
-      id: 1,
-      content: "What tweeterific tweet",
-      posted_at: Date.now(),
-      creatorName: "Ayn Rand",
-      creatorHandle: "@AtlasShrugged",
-      avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Ayn_Rand_by_Talbot_1943.jpg/220px-Ayn_Rand_by_Talbot_1943.jpg",
-      replyCount: 4,
-      retweets: 5,
-      likes: 120
-    },
-    {
-      id: 2,
-      content: "I like apples, but not oranges.",
-      posted_at: Date.now(),
-      creatorName: "Ayn Rand",
-      creatorHandle: "@AtlasShrugged",
-      avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Ayn_Rand_by_Talbot_1943.jpg/220px-Ayn_Rand_by_Talbot_1943.jpg",
-      replyCount: 345,
-      retweets: 500,
-      likes: 2
-    }
-  ];
-
-  const userProfile = {
-    id: "12345",
-    creatorName: "John Doe",
-    avatar: "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2017/04/04/604513.jpg?itok=FqqfYOfA",
-    posted_at: Date.now(),
-    creatorHandle: "@JohnDoe"
-  }
-
-  const [timeline, setTimeline] = useState();
-
-  useEffect(() => {
-    setTimeline(TWEETS);
-  }, []);
-
-  const handleTweetSubmit = (submittedTweet) => {
-    let tweet = {
-      ...userProfile,
-      content: submittedTweet,
-      replyCount: 0,
-      retweets: 0,
-      likes: 0
-    };
-    setTimeline((prev) => [tweet, ...prev])
-  }
-
+const [timeline, handleTweetSubmit, userProfile] = useTweetInteract();
 
   return (
     <Router>
---
+++ src/hooks/tweet-interact-hook.js
@@ -0,0 +1,56 @@
+import { useState, useCallback, useEffect } from "react";
+
+
+
+export const useTweetInteract = (() => {
+  const userProfile = {
+    id: "12345",
+    creatorName: "John Doe",
+    avatar: "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2017/04/04/604513.jpg?itok=FqqfYOfA",
+    posted_at: Date.now(),
+    creatorHandle: "@JohnDoe"
+  }
+  const TWEETS = [
+    {
+      id: 1,
+      content: "What tweeterific tweet",
+      posted_at: Date.now(),
+      creatorName: "Ayn Rand",
+      creatorHandle: "@AtlasShrugged",
+      avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Ayn_Rand_by_Talbot_1943.jpg/220px-Ayn_Rand_by_Talbot_1943.jpg",
+      replyCount: 4,
+      retweets: 5,
+      likes: 120
+    },
+    {
+      id: 2,
+      content: "I like apples, but not oranges.",
+      posted_at: Date.now(),
+      creatorName: "Ayn Rand",
+      creatorHandle: "@AtlasShrugged",
+      avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Ayn_Rand_by_Talbot_1943.jpg/220px-Ayn_Rand_by_Talbot_1943.jpg",
+      replyCount: 345,
+      retweets: 500,
+      likes: 2
+    }
+  ];
+  const [timeline, setTimeline] = useState();
+
+  useEffect(() => {
+    setTimeline(TWEETS);
+  }, []);
+
+
+  const handleTweetSubmit = useCallback((submittedTweet) => {
+    let tweet = {
+      ...userProfile,
+      content: submittedTweet,
+      replyCount: 0,
+      retweets: 0,
+      likes: 0
+    };
+    setTimeline((prev) => [tweet, ...prev])
+  }, []);
+  
+  return [timeline, handleTweetSubmit, userProfile];
+})
