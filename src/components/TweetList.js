a30bdb266137cff26afa955aae4e1572037eaae5
Jad K <jad_dis@hotmail.com>
2020-03-24 07:12:44 -0400

    got replies rendering on screen

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -2,19 +2,45 @@ import React from "react";
 import Tweet from "./Tweet";
 
 const TweetList = (props) => (
-  props.timeline.map((tweet) => (
-      <Tweet 
-      key={tweet.id}
-      id ={tweet.id} 
-      creatorName={tweet.creatorName} 
-      avatar={tweet.avatar}
-      content={tweet.content} 
-      posted_at={tweet.posted_at}
-      creatorHandle={tweet.creatorHandle}
-      likes={tweet.likes}
-      retweets={tweet.retweets}
-      replyCount={tweet.replyCount}
-      />
-  ))
+  props.timeline.map((tweet) => {
+    return (
+      <React.Fragment>
+        <Tweet
+          key={tweet.id}
+          id={tweet.id}
+          creatorName={tweet.creatorName}
+          avatar={tweet.avatar}
+          content={tweet.content}
+          posted_at={tweet.posted_at}
+          creatorHandle={tweet.creatorHandle}
+          likes={tweet.likes}
+          retweets={tweet.retweets}
+          replyCount={tweet.replyCount}
+        />
+        {tweet.replies.length > 0 && (
+          tweet.replies.map((tweet) => (
+            <React.Fragment>
+              <div className="reply-line"> </div>
+              <Tweet
+                key={tweet.id}
+                id={tweet.id}
+                creatorName={tweet.creatorName}
+                avatar={tweet.avatar}
+                content={tweet.content}
+                posted_at={tweet.posted_at}
+                creatorHandle={tweet.creatorHandle}
+                likes={tweet.likes}
+                retweets={tweet.retweets}
+                replyCount={tweet.replyCount}
+              />
+            </React.Fragment>
+          )
+          )
+        )}
+      </React.Fragment>
+
+    )
+  }
+  )
 )
 export default TweetList;
