988ce334f83d747e6bf51332868e3c4c2b6d90d2
Jad K <jad_dis@hotmail.com>
2020-04-17 04:30:28 -0400

    implemented back button functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,31 @@ import React from "react";
 import "./Header.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faCircle } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 const Header = (props) => {
-
-  const clickHandler = () => {
-
+  let history = useHistory();
+  const backButtonHandler = () => {
+    history.goBack();
   }
   return (
     <div className="header">
-    {props.navButton && (
-      <span className="fa-layers back-button" onClick={clickHandler}>
-        <FontAwesomeIcon
-          icon={faArrowLeft}
-          size="lg"
-          color="#1da1f2"
-          className="back-icon"
-        />
-        <FontAwesomeIcon
-          icon={faCircle}
-          size="3x"
-          color="rgba(29, 161, 242, 0.1)"
-          className="back-circle-icon"
-          transform="left-3.0"
-        />
-      </span>
-    )}
+      {props.navButton && (
+        <span className="fa-layers back-button" onClick={backButtonHandler}>
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            size="lg"
+            color="#1da1f2"
+            className="back-icon"
+          />
+          <FontAwesomeIcon
+            icon={faCircle}
+            size="3x"
+            color="rgba(29, 161, 242, 0.1)"
+            className="back-circle-icon"
+            transform="left-3.0"
+          />
+        </span>
+      )}
       <div className="header-text">
         <h2>{props.title}</h2>
         <div className="subheader">
