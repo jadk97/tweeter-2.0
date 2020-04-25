f9da08d53a548c504f69abc7cd440d3989800579
Jad K <jad_dis@hotmail.com>
2020-04-25 05:56:49 -0400

    fixed a bug related to how replies were being stored in the userProfile state

--- src/reducers/userProfileSlice.js
+++ src/reducers/userProfileSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-
+import findPath from "../helpers/findPath";
+import _ from "lodash";
 
 
 export const slice = createSlice({
@@ -28,7 +28,34 @@ export const slice = createSlice({
       state.likedTweets.splice(tweetToRemove, 1);
     },
     addTweet: (state, action) => {
-      state.tweets.unshift(action.payload);
+      console.log("addtweet was called");
+      if (action.payload.type === "child" && action.payload.mentions.includes(state.creatorHandle)) {
+        console.log("its hittin");
+        let tweetRepliedTo = action.payload.replyingTo[action.payload.replyingTo.length - 1];
+        console.log("tweetRepliedTo in userProfile ", tweetRepliedTo);
+        let pathToTweet = findPath(tweetRepliedTo, state.tweets).split(".");
+        console.log("THE CURRENT USER PATH TO TWEET IS:", _.get(state.tweets, pathToTweet));
+
+        // Make sure to stress test this later
+        let parentTweet = state.tweets.findIndex((tweet) => tweet.id === action.payload.replyingTo[0]);
+        if (parentTweet === -1) {
+          parentTweet = state.tweets.findIndex((tweet) => tweet.id === tweetRepliedTo)
+        }
+        console.log("THIS IS THE PARENT TWEET: ", parentTweet);
+        if (state.tweets[parentTweet].id !== _.get(state.tweets, pathToTweet)) {
+          state.tweets[parentTweet].replyCount++;
+
+        }
+        pathToTweet[pathToTweet.length - 1] = "replies";
+
+
+        _.set(state.tweets, pathToTweet, [..._.get(state.tweets, pathToTweet), action.payload]);
+        pathToTweet[pathToTweet.length - 1] = "replyCount";
+        _.set(state.tweets, pathToTweet, _.get(state.tweets, pathToTweet) + 1);
+      }
+      else {
+        state.tweets.unshift(action.payload);
+      }
     },
     removeTweet: (state, action) => {
       let tweetToRemove = state.tweets.findIndex((tweet) => tweet.id === action.payload.id);
@@ -41,7 +68,7 @@ export const slice = createSlice({
     removeRetweet: (state, action) => {
       let retweetToRemove = state.retweetedTweets.findIndex((tweet) => tweet.id === action.payload.id);
       state.retweetedTweets.splice(retweetToRemove, 1);
-      retweetToRemove = state.tweets.findIndex((tweet) => tweet.id === action.payload.id );
+      retweetToRemove = state.tweets.findIndex((tweet) => tweet.id === action.payload.id);
       state.tweets.splice(retweetToRemove, 1);
     }
   }
