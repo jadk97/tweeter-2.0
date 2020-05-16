b45ae56791d8a797778ed0e681d672f073c71964
Jad K <jad_dis@hotmail.com>
2020-05-16 06:00:27 -0400

    fixed a bug that would cause the Notifications component to bug out when a user retweets a tweet with  children

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -4,7 +4,7 @@ import flattenTweets from "../helpers/flattenTweets";
 import { useSelector } from "react-redux";
 import { selectUserProfile } from "../reducers/userProfileSlice";
 import { useRouteMatch, useLocation } from "react-router-dom";
-
+import _ from "lodash";
 import HorizontalNavBar from "./HorizontalNavBar";
 import NotificationList from "./NotificationList";
 const Notifications = (props) => {
@@ -12,44 +12,53 @@ const Notifications = (props) => {
   const userProfile = useSelector(selectUserProfile);
   let match = useRouteMatch();
   let location = useLocation();
-
-  // const getInteractions = (userProfile.tweets || []).map((tweet) => {
-  //   if (tweet.likedBy.length > 0 || tweet.retweetedBy.length > 0 || (tweet.type === "child" && tweet.creatorHandle !== userProfile.creatorHandle)) {
-  //     return tweet;
-  //   }
-
-  // });
   let viewMode = location.pathname.split("/");
-  const getInteractions = [...userProfile.tweets];
 
+  let retweetCheck = userProfile.tweets.filter((tweet) => tweet.retweetedBy.indexOf(userProfile.creatorHandle) > - 1 && tweet.replyCount > 0);
+  console.log("retweetCheck", retweetCheck);
+  let getInteractions = [...userProfile.tweets];
 
-  let flattenedInteractions = [...flattenTweets(getInteractions)];
-  let notificationsFeed = [];
-  
-  for(let interaction of flattenedInteractions){
-    if(interaction.likedBy.length > 0 && interaction.retweetedBy.length > 0){
-      notificationsFeed.push({notifType: "like", ...interaction});
-      notificationsFeed.push({notifType: "retweet", ...interaction});
-    }
-    else if(interaction.likedBy.length > 0){
-      notificationsFeed.push({notifType: "like", ...interaction});
-    }
-    else if(interaction.retweetedBy.length > 0){
-      notificationsFeed.push({notifType: "retweet", ...interaction});
-    }
-    else{
-      notificationsFeed.push({notifType: "reply", ...interaction})
+  if(retweetCheck.length > 0){
+    for (let retweet of retweetCheck){
+      let tweetIndex = getInteractions.findIndex((tweet) => retweet.id === tweet.id);
+      getInteractions[tweetIndex] = {...getInteractions[tweetIndex], replies: []};
     }
   }
 
-  if(viewMode[viewMode.length - 1] === "mentions"){
+  let flattenedInteractions = [...flattenTweets(getInteractions)];
+  console.log("FLATTENED INTERACTIONS", flattenedInteractions);
+  let notificationsFeed = [];
+
+  for (let interaction of flattenedInteractions) {
+    if(interaction.creatorHandle === userProfile.creatorHandle){
+      if (interaction.likedBy.length > 0 && interaction.retweetedBy.length > 0) {
+        notificationsFeed.push({ notifType: "like", ...interaction });
+        notificationsFeed.push({ notifType: "retweet", ...interaction });
+      }
+      else if (interaction.likedBy.length > 0) {
+        notificationsFeed.push({ notifType: "like", ...interaction });
+      }
+      else if (interaction.retweetedBy.length > 0) {
+        notificationsFeed.push({ notifType: "retweet", ...interaction });
+      }
+    }
+    else if (userProfile.retweetedTweets.some((tweet) => interaction.id === tweet.id)){
+      console.log("this is a retweet the user made", interaction);
+
+    }
+    else {
+      notificationsFeed.push({ notifType: "reply", ...interaction })
+    }
+  }
+
+  if (viewMode[viewMode.length - 1] === "mentions") {
     notificationsFeed = notificationsFeed.filter((notif) => notif.notifType === "reply");
   }
   console.log(notificationsFeed);
   return (
     <div className="center-view">
-      <Header title={"Notifications"} noBorder={true}/>
-      <HorizontalNavBar headings={[{ title: "All", path: `${match.url}` }, {title: "Mentions", path: `${match.url}/mentions`}]} />
+      <Header title={"Notifications"} noBorder={true} />
+      <HorizontalNavBar headings={[{ title: "All", path: `${match.url}` }, { title: "Mentions", path: `${match.url}/mentions` }]} />
       <NotificationList notifications={notificationsFeed} />
     </div>
   )
