9ce8eda5f2888de1d4fade36565ddb4b17213e16
Jad K <jad_dis@hotmail.com>
2020-05-23 02:48:45 -0400

    created an external data file for rendering mention suggestions on screen within TweetInput

---
+++ src/components/TweetInput/mentions.js
@@ -0,0 +1,18 @@
+const mentions = [
+  {
+    title: 'Ayn Rand',
+    name: 'AtlasShrugged',
+    avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Ayn_Rand_by_Talbot_1943.jpg/220px-Ayn_Rand_by_Talbot_1943.jpg',
+  },
+  {
+    title: "Ernest Hemingway",
+    name: 'EHemWay',
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/ErnestHemingway.jpg/220px-ErnestHemingway.jpg',
+  },
+  {
+    title: 'John Doe',
+    name: 'JohnDoe',
+    avatar: 'https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2017/04/04/604513.jpg?itok=FqqfYOfA',
+  }
+];
+export default mentions;
