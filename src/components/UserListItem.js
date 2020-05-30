6d3e56e8ba6afd6b387d58704150556373b64aa3
Jad K <jad_dis@hotmail.com>
2020-05-30 04:18:23 -0400

    userListItem now gives you a visual indication if the user that liked your tweet also follows you

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./UserListItem.css";
+import { selectUserProfile, followUser, unfollowUser } from '../reducers/userProfileSlice';
+import { useDispatch, useSelector } from "react-redux";
 
 const UserListItem = (props) => {
-  
-
+  const dispatch = useDispatch();
+  const userProfile = useSelector(selectUserProfile);
+  let isFollowed = userProfile.following.filter((user) => user.creatorHandle === props.creatorHandle).length > 0;
+  let followsYou = !!userProfile.followers.find((user) => user.creatorHandle === props.creatorHandle);
   return (
     <div className="user-container">
       <div className="user-details">
@@ -14,7 +18,7 @@ const UserListItem = (props) => {
           <div className="creator-details">
             <span className="creator-credentials">
               <span className="creator-name">{props.creatorName}</span>
-              <div className="creator-handle"> @{props.creatorHandle}</div>
+              <div className="creator-handle"> @{props.creatorHandle} {followsYou && <span className="profile-follows-you">Follows you</span>}</div>
             </span>
           </div>
           <div className="user-bio">
