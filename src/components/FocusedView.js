74203a4eea9cf2397a07ad0bc84aa0436b0723db
Jad K <jad_dis@hotmail.com>
2020-05-30 05:45:01 -0400

    modified InteractionView so that reply, like, and retweet counts remain up to date

--- src/components/FocusedView.js
+++ src/components/FocusedView.js
@@ -13,18 +13,14 @@ import _ from "lodash";
 const FocusedView = (props) => {
   let { creatorHandle, id } = useParams();
   const timeline = useSelector(selectTimeline);
-  // console.log("THIS IS THE TIMELINE INSIDE OF FOCUSEDVIEW", timeline);
-  // console.log(id);
+
   
   let pathToTweet = findPath(id, timeline).split(".");
 
-  // console.log("This is the path to tweet inside FocusedView", pathToTweet);
   pathToTweet.pop();
-  // console.log("This is the path to tweet inside FocusedView", pathToTweet);
-  // console.log(_.get(timeline, pathToTweet));
+
   let tweetToFocus = _.get(timeline, pathToTweet);
-  // console.log("this is the tweetToFocus: ", tweetToFocus);
-  // let parentTweetReplies=findPath(timeline, pathToTweet[0]);
+ 
   let pathToChildTweet;
   let replyChainToRender;
  
@@ -36,47 +32,13 @@ const FocusedView = (props) => {
       individualTweets.push(_.get(timeline, pathCopy));
     }
   }
-  // console.log("this is indvidual tweets", individualTweets);
+  
   replyChainToRender  = individualTweets.reduceRight(function (a, c) {
       return {
         ...c,
         replies: [a]
       }; 
     });
-// console.log(replyChainToRender);
-  
-  // if (tweetToFocus.type === "child") {
-  //   console.log("tweet is a child");
-  //   console.log("The last index of tweetToFocus: ", tweetToFocus.replyingTo[tweetToFocus.replyingTo.length - 1]);
-  //   pathToChildTweet = findPath(tweetToFocus.replyingTo[tweetToFocus.replyingTo.length - 1], timeline).split(".");
-  //   pathToChildTweet.pop();
-  //   parentTweet = _.get(timeline, pathToChildTweet);
-
-  //   let rootParentTweet = _.get(timeline, pathToTweet[0]);
-  //   console.log("This is the rootParentTweet:", rootParentTweet)
-  //   console.log("This is the parent tweet inside of FocusedView: ", parentTweet);
-  //   console.log("TWEET TO FOCUS: ", tweetToFocus);
-  //   let tweetIndex = parentTweet.replies.findIndex((tweet) => tweet.id === tweetToFocus.id);
-  //   console.log("this is the index of the child tweet", tweetIndex);
-  //   if(tweetIndex > 0) {
-  //     console.log("This is the replies of parent tweet inside of Focusedview:", parentTweet.replies);
-  //     // let tweetChecker = parentTweet;
-  //     console.log("This is the root parent tweet: ", rootParentTweet);
-  //     console.log("UNGA");
-  //     // replyChainToRender = {...parentTweet, replies: [tweetToFocus] }
-  //     parentTweet.id === rootParentTweet.id ? (replyChainToRender =  {...parentTweet, replies: [tweetToFocus]})
-  //      : (replyChainToRender = {...rootParentTweet, replies: [{ ...parentTweet, replies: [tweetToFocus]}]})
-  //   }
-  //   else {
-  //     parentTweet.id === rootParentTweet.id ? (replyChainToRender =  {...parentTweet, replies: [tweetToFocus]})
-  //      : (replyChainToRender = {...rootParentTweet, replies: [{ ...parentTweet, replies: [tweetToFocus]}]})
-  //     // replyChainToRender = parentTweet;
-  //   }
-
-    
-  //   // console.log("TWEET TO FOCUS replyCount: ",   tweetToFocus.replies.length)
-  //   console.log("this is the replychaintorender", replyChainToRender);
-  // }
 
   return (
     <div className="center-view">
--- src/components/InteractionView.js
+++ src/components/InteractionView.js
@@ -3,25 +3,37 @@ import Header from "./Header";
 import Tweet from "./Tweet";
 import { useLocation, Redirect, useHistory } from "react-router-dom";
 import UserList from "./UserList";
+import _ from "lodash";
+import findPath from "../helpers/findPath";
+import { useSelector } from "react-redux";
+import { selectUserProfile } from '../reducers/userProfileSlice';
 const InteractionView = (props) => {
 
   const location = useLocation();
   const history = useHistory();
+  const userProfile = useSelector(selectUserProfile);
+  let tweetToFocus;
 
-  
   const subtitleFormatter = (notifUsers) => {
     let userNames = notifUsers.map((user) => user.creatorName);
     if (userNames.length > 2) {
       return `By ${userNames[0]} and ${userNames.length - 1} others`;
     }
     else {
-       return "By " + userNames.join(" and ");
+      return "By " + userNames.join(" and ");
     }
   }
 
   let formattedSubtitle;
   if (location.state) {
     formattedSubtitle = subtitleFormatter(location.state.notifUsers);
+
+    let pathToTweet = findPath(location.state.tweetID, userProfile.tweets).split(".");
+    pathToTweet.pop();
+    let tweetCopy =  _.get(userProfile.tweets, pathToTweet);
+    tweetToFocus = {...tweetCopy, replies: []}
+   
+    
   }
   else {
     history.push("/home");
@@ -31,13 +43,13 @@ const InteractionView = (props) => {
     <div className="center-view">
       {location.state && (
         <React.Fragment>
-        <Header
-          navButton={true}
-          title={location.state.notifType === "like" ? "Liked" : "Retweeted"}
-          subtitle={formattedSubtitle}
-        />
-        <Tweet id={location.state.tweetID} tweet={location.state.tweet} focusedView={true}/>
-        <UserList notifUsers={location.state.notifUsers} />
+          <Header
+            navButton={true}
+            title={location.state.notifType === "like" ? "Liked" : "Retweeted"}
+            subtitle={formattedSubtitle}
+          />
+          <Tweet id={location.state.tweetID} tweet={tweetToFocus} />
+          <UserList notifUsers={location.state.notifUsers} />
         </React.Fragment>
       )}
 
